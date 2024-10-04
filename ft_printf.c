2f1fb244039ad63ddd21785c4dbdc4498ab28795
katakada <katakada@student.42tokyo.jp>
2024-10-04 14:34:56 +0900

    Refactor ft_printf.c: Fix null format check in ft_vdprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,36 @@
 /*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:11:34 by katakada          #+#    #+#             */
-/*   Updated: 2024/09/18 20:13:28 by katakada         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:17:50 by katakada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+// ft_vdprintf requires format not to be null
 int	ft_vdprintf(int fd, const char *format, va_list *args)
 {
 	int		count;
+	int		cnt_tmp;
 	t_flags	flags;
 
-	if (!format)
-		return (-1);
 	count = 0;
 	while (*format != '\0')
 	{
 		ft_bzero(&flags, sizeof(flags));
 		if (*format == '%')
-			count += print_format(&format, &flags, args, fd);
+		{
+			cnt_tmp = print_format(&format, &flags, args, fd);
+			if (cnt_tmp < 0)
+				return (-1);
+			count += cnt_tmp;
+		}
 		else
 		{
 			if (ft_putchar_fd(*format, fd) < 0)
 				return (-1);
 			count++;
 		}
-		if (count < 0)
-			return (-1);
 		format++;
 	}
 	return (count);
